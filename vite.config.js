f82d00b14047c3d94f98c5bd6e9213f4358e35a8
Yujie <13761990472@163.com>
2025-07-19 21:03:19 +0800

    change path

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: 'dynamic-table',
+  build: {
+    outDir: 'docs'            // ← 直接输出到 docs 目录
+  },
   plugins: [vue()]
 }) 
